837c8d921197f95b762001bcbcae3471dd06befb
Fabian De la cruz <fabianjo0316@gmail.com>
2024-04-23 18:35:31 -0500

    Actualizacion primera tabla

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -11,9 +13,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
    username: 'root',
    password: '03160606',
    database: 'api_cul',
-   entities: [],
+   entities: [User],
    synchronize: true,
-  })],
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
